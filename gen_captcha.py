868feef363edc710daca7ba471d9e38e4fd3bb0e
deJQK <jinqingking@gmail.com>
2017-04-03 18:31:11 +0800

    Add Flags for digit, lower and upper cases, as well as number of chars per image

--- gen_captcha.py
+++ gen_captcha.py
@@ -7,9 +7,7 @@ from captcha.image import ImageCaptcha
 
 import itertools
 
-
 CHOICES = map(str, list(range(10)) + list(string.ascii_lowercase))
-
 
 def one_char(n=1000, img_dir='images'):
     if not os.path.exists(img_dir):
@@ -24,19 +22,30 @@ def one_char(n=1000, img_dir='images'):
             image.write(str(i), fn)
 
 
-def _gen_captcha(n, num_per_image, img_dir='images'):
+def _gen_captcha(n=1000, num_per_image=1, img_dir='images', d=True, l=False, u=False):
     if not os.path.exists(img_dir):
         os.makedirs(img_dir)
 
     image = ImageCaptcha(width=40 + 20 * num_per_image, height=100)
     print 'generating %s captchas in %s' % (n, img_dir)
 
-    for _ in range(n):
-        for i in itertools.permutations(CHOICES, num_per_image):
-            captcha = ''.join(i)
-            fn = os.path.join(img_dir, '%s_%s.png' % (captcha, uuid.uuid4()))
-            image.write(captcha, fn)
-
+    if not (d or l or u):
+        print 'Digit, lower case and upper case cannot be all False.'
+        return
+    else:
+        label_list = []
+        if d:
+            label_list = label_list + list(range(10))
+        if l:
+            label_list = label_list + list(string.ascii_lowercase)
+        if u:
+            label_list = label_list + list(string.ascii_uppercase)
+        label_set = map(str, label_list)
+        for _ in range(n):
+            for i in itertools.permutations(label_set, num_per_image):
+                captcha = ''.join(i)
+                fn = os.path.join(img_dir, '%s_%s.png' % (captcha, uuid.uuid4()))
+                image.write(captcha, fn)
 
 if __name__ == '__main__':
 
@@ -51,10 +60,36 @@ if __name__ == '__main__':
         default=0.2,
         type=float,
         help='ratio of test / train.')
+    parser.add_argument(
+        '-d',
+        default=True,
+        type=bool,
+        help='digits as labels.')
+    parser.add_argument(
+        '-l',
+        default=True,
+        type=bool,
+        help='lower case chars as labels.')
+    parser.add_argument(
+        '-u',
+        default=True,
+        type=bool,
+        help='upper case chars as labels.')
+    parser.add_argument(
+        '--npi',
+        default=1,
+        type=int,
+        help='number of characters per image.')
 
     FLAGS, unparsed = parser.parse_known_args()
     train_number = FLAGS.n
     test_number = int(FLAGS.n * FLAGS.t)
+    digit = FLAGS.d
+    lower = FLAGS.l
+    upper = FLAGS.u
+    num_per_image=FLAGS.npi
 
-    _gen_captcha(train_number, 4, 'images/char4/train')
-    _gen_captcha(test_number, 4, 'images/char4/test')
+    _gen_captcha(n=train_number, num_per_image=num_per_image,
+            img_dir='images/char'+str(num_per_image)+'/train', d=digit, l=lower, u=upper)
+    _gen_captcha(n=test_number, num_per_image=num_per_image,
+            img_dir='images/char'+str(num_per_image)+'/test', d=digit, l=lower, u=upper)
