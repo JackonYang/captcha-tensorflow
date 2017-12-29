a38ce48baffd3cd15785cea02a2defe640b66022
Jackon Yang <i@jackon.me>
2017-12-30 01:35:38 +0800

    fix CNN

--- conv_captcha.py
+++ conv_captcha.py
@@ -42,7 +42,7 @@ def conv2d(x, W):
 
 
 def max_pool_2x2(x):
-    return tf.nn.avg_pool(x, ksize=[1, 5, 5, 1],
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                           strides=[1, 2, 2, 1], padding='SAME')
 
 
@@ -69,17 +69,17 @@ def main(_):
 
     # define the model
     with tf.name_scope('convolution-layer-1'):
-        W_conv1 = weight_variable([7, 7, 1, 32])
+        W_conv1 = weight_variable([5, 5, 1, 32])
         b_conv1 = bias_variable([32])
 
-        h_conv1 = tf.nn.tanh(conv2d(x_image, W_conv1) + b_conv1)
+        h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
         h_pool1 = max_pool_2x2(h_conv1)
 
     with tf.name_scope('convolution-layer-2'):
-        W_conv2 = weight_variable([7, 7, 32, 64])
+        W_conv2 = weight_variable([5, 5, 32, 64])
         b_conv2 = bias_variable([64])
 
-        h_conv2 = tf.nn.tanh(conv2d(h_pool1, W_conv2) + b_conv2)
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
         h_pool2 = max_pool_2x2(h_conv2)
 
     with tf.name_scope('densely-connected'):
@@ -87,7 +87,7 @@ def main(_):
         b_fc1 = bias_variable([1024])
 
         h_pool2_flat = tf.reshape(h_pool2, [-1, IMAGE_WIDTH*IMAGE_HEIGHT*4])
-        h_fc1 = tf.nn.tanh(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+        h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     with tf.name_scope('dropout'):
         # To reduce overfitting, we will apply dropout before the readout layer
